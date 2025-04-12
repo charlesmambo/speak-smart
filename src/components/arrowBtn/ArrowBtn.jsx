9104dbe5c009e7290ca46736b1f1da1f75b1d726
Charles-M <chylah11st@gmail.com>
2025-04-13 01:28:32 +0200

    complete intro section mobile and desktop

---
+++ src/components/arrowBtn/ArrowBtn.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../arrowBtn/ArrowBtn.css' ;
+import { MdArrowForward } from "react-icons/md";
+
+
+const ArrowBtn = () => {
+  return (
+    <div className='arrow-btn'>
+        <MdArrowForward/>
+    </div>
+  )
+}
+
+export default ArrowBtn
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import '../intro/Intro.css'
+import PrimaryBtn from '../buttons/primaryBtn'
+import ArrowBtn from '../arrowBtn/ArrowBtn'
 
 const Intro = () => {
   return (
     <div className='intro'>
-      <h1>Ropa</h1>
+      <div className="intro-heading">
+        <h1>Personalized Tutoring That Goes Beyond Grades</h1>
+      </div>
+
+      <div className="intro-content">
+        <p>At Speak Smart Tutoring, we believe that when learning is engaging, stress-free, and exciting, students develop a love for education that lasts a lifetime. We are more than just a tutoring service—we are partners in your child’s academic success!</p>
+
+        <div className="intro-btn-container">
+           <PrimaryBtn text="book demo" className="intro-btn" />
+           <ArrowBtn/>
+        </div>
+      </div>
     </div>
   )
 }
